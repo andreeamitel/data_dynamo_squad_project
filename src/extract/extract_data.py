23f31bf87bcaeae3c16d61ca75dfb6daf53bb09b
Andreea Mitel <andreeamitel@Andreeas-MacBook-Pro.local>
2024-02-13 15:27:15 +0000

    furter tests for extract_data

--- src/extract/extract_data.py
+++ src/extract/extract_data.py
@@ -11,6 +11,13 @@ def extract_data(table_name, db_conn):
     A list of dictionaries of all the data in the table input.
   
     """
-    rows = db_conn.run(f"SELECT * FROM table WHERE {table_name}")
+    rows = db_conn.run(f"SELECT * FROM {table_name};")
+    data = [{
+            "currency_id": row[0],
+            "currency_code": row[1],
+            "created_at": row[2],
+            "updated_at": row[3]
+            }
+            for row in rows]
+    return data
     
-    return [{}]
