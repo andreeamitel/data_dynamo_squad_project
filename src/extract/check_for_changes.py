fa67a38190c1c5b29824aafa9008a20b3e14fc1b
Sumeyah <sumiibra17@icloud.com>
2024-02-13 17:39:30 +0000

    add check_table_for_last_updated() and get_last_ingested_time() in check_for_changes_file

--- src/extract/check_for_changes.py
+++ src/extract/check_for_changes.py
@@ -1,10 +1,26 @@
 import boto3
 from  pprint import pprint
 from connection import conn
+import datetime
 def check_for_changes(db_conn):
+    date_time = get_last_ingested_time()
+
+    address_last_updated = conn.run('SELECT last_updated FROM address;')
+
+    return check_table_for_last_updated(address_last_updated, 'address', date_time)
+
+
+
+
+def check_table_for_last_updated(table_lists, table_name, last_ingested_time):
+    for time in table_lists:
+        if time != last_ingested_time:
+            return table_name
+
+def get_last_ingested_time():
     s3 = boto3.client('s3')
     response = s3.get_object(
-        SortOrder='asc'
-        )
-    result = db_conn.run('SELECT * FROM sales_order;')
-    return result
+    Bucket='ingested-bucket-20240213151611822700000004',
+    Key='last-ingested-time.txt',
+    )
+    pprint(response['Body'].read())
