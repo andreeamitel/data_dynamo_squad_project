08f9f1ae113d31496544ce0117f287558416933f
Sumeyah <sumiibra17@icloud.com>
2024-02-13 17:39:30 +0000

    add check_table_for_last_updated() and get_last_ingested_time() in check_for_changes_file

--- test/test_check_for_changes.py
+++ test/test_check_for_changes.py
@@ -1,10 +1,17 @@
 from src.extract.check_for_changes import check_for_changes
 from unittest.mock import patch
 from pg8000.native import Connection
+import datetime
 
 @patch('src.extract.check_for_changes.conn')
-def test_connects_to_database(mock_conn):
+def test_conn_run_has_been_called(mock_conn):
     mock_conn.run.return_value = [[1, 2, 3, 4]]
+    check_for_changes(mock_conn)
+    mock_conn.run.assert_called_once()
+
+@patch('src.extract.check_for_changes.conn')
+def test_checks_for_changes_in_address_table(mock_conn):
+    mock_conn.run.return_value = [datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)]
     result = check_for_changes(mock_conn)
-    expected = [[1, 2, 3, 4]]
+    expected = 'address'
     assert result == expected
