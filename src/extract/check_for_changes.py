29dc43eaaf30484bd5fdabba85e359378e4f9019
Valerie Parfeliuk <parfelyuk.v@gmail.com>
2024-02-14 14:13:50 +0000

    added check_for_changes and check_table_for_last_updated functions

---
+++ src/extract/check_for_changes.py
@@ -0,0 +1,42 @@
+import boto3
+from pprint import pprint
+from connection import conn, identifier, literal, DatabaseError
+from datetime import datetime
+
+
+def check_for_changes(db_conn, last_ingested_time):
+    try:
+        format = "%Y-%m-%d %H:%M:%S.%f"
+        date_time = datetime.strptime(last_ingested_time, format)
+        table_names = [
+            "address",
+            "staff",
+            "design",
+            "sales_order",
+            "currency",
+            "department",
+            "counterparty",
+        ]
+        result = [
+            table
+            for table in table_names
+            if check_table_for_last_updated(table, date_time, db_conn)
+        ]
+
+        return result
+    except TypeError as error:
+        raise error
+       
+
+
+def check_table_for_last_updated(table_name, last_ingested_time, conn):
+    try:
+        table_lists = conn.run(f"SELECT last_updated FROM {identifier(table_name)};")
+        for time in table_lists:
+            if time[0] != last_ingested_time:
+                return True
+    except DatabaseError as error:
+        print(error, f"Something wrong with query.")
+        raise error
+
+
