239f5803845701358760be4f5ab8f683273d014a
Andreea Mitel <andreeamitel@Andreeas-MacBook-Pro.local>
2024-02-14 15:23:54 +0000

    workflow test

--- src/extract/extract_data.py
+++ src/extract/extract_data.py
@@ -1,5 +1,3 @@
-from pg8000.exceptions import DatabaseError
-
 def extract_data(table_name, db_conn, last_ingested_time):
     """
     This function extracts the data from a table in totesys database.
@@ -11,16 +9,16 @@ def extract_data(table_name, db_conn, last_ingested_time):
 
     Returns:
     A list of dictionaries of all the data in the table input.
-  
+
     """
     try:
         rows = db_conn.run(f"""
             SELECT array_to_json(array_agg({table_name}), FALSE) AS table_dict
             FROM {table_name}
-            WHERE created_at > '{last_ingested_time}' OR last_updated > '{last_ingested_time}';
+            WHERE created_at > '{last_ingested_time}'
+            OR last_updated > '{last_ingested_time}';
             """)
         data = rows[0][0]
         return data
     except Exception:
         return "Error when running SQL query in extract_data function"
-    
