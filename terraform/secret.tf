b5771a4e12c81078ec1a9e5dd962cfc7eae40444
Andreea Mitel <andreeamitel@Andreeas-MacBook-Pro.local>
2024-02-14 16:04:45 +0000

    workflow test

--- terraform/secret.tf
+++ terraform/secret.tf
@@ -8,6 +8,6 @@ resource "aws_secretsmanager_secret" "database_creds_secret" {
 # need to input database creds each time run terraform plan and apply, 
 # will be set up to auto when workflow set up
 resource "aws_secretsmanager_secret_version" "database_creds_values" {
-  secret_id     = aws_secretsmanager_secret.example_secret.id
+  secret_id     = aws_secretsmanager_secret.database_creds_secret.id
   secret_string = var.database_creds_var
 }
