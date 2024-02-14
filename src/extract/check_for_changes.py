2c02b2aeb090821188e8ca58eb90ba0e045d5fbd
Valerie Parfeliuk <parfelyuk.v@gmail.com>
2024-02-14 15:39:46 +0000

    refactored check_table_for_last_updated, added test file

--- src/extract/check_for_changes.py
+++ src/extract/check_for_changes.py
@@ -26,17 +26,16 @@ def check_for_changes(db_conn, last_ingested_time):
         return result
     except TypeError as error:
         raise error
-       
 
 
 def check_table_for_last_updated(table_name, last_ingested_time, conn):
     try:
         table_lists = conn.run(f"SELECT last_updated FROM {identifier(table_name)};")
         for time in table_lists:
+            print(time[0], last_ingested_time)
             if time[0] != last_ingested_time:
                 return True
+        return False
     except DatabaseError as error:
-        print(error, f"Something wrong with query.")
+        print(f"Something wrong with query: {error.args[0]['M']}")
         raise error
-
-
