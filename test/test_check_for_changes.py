906e3504bd920b09387285bca377e4c495722676
Sumeyah <sumiibra17@icloud.com>
2024-02-14 10:01:48 +0000

    update check_for_changes() and its utils

--- test/test_check_for_changes.py
+++ test/test_check_for_changes.py
@@ -13,5 +13,12 @@ def test_conn_run_has_been_called(mock_conn):
 def test_checks_for_changes_in_address_table(mock_conn):
     mock_conn.run.return_value = [datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)]
     result = check_for_changes(mock_conn)
-    expected = 'address'
+    expected = ['address']
+    assert result == expected
+
+@patch('src.extract.check_for_changes.conn')
+def test_checks_for_changes_in_all_tables(mock_conn):
+    mock_conn.run.return_value = [datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)]
+    result = check_for_changes(mock_conn)
+    expected = ['address']
     assert result == expected
