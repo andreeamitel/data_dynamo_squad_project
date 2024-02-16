e45f29fe40e29885401b57542df7f2090b4f08bd
Sumeyah <sumiibra17@icloud.com>
2024-02-16 12:12:01 +0000

    add zip file and backend configuration

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,9 +2,8 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-# data "archive_file" "ingest_lambda" {
-#   type        = "zip"
-# #   change source_file later after creating py file
-#   source_file = "../src/reader.py"
-#   output_path = "ingest_lambda.zip"
-# }
+data "archive_file" "ingest_lambda" {
+  type        = "zip"
+  source_dir = "../src/extract"
+  output_path = "./ingest_lambda.zip"
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region= "eu-west-2"
 }
+
+terraform {
+   backend "s3" {
+    bucket = "backend-project-bucket"
+    key = "data/tfstate"
+    region = "eu-west-2"
+   }
+}
