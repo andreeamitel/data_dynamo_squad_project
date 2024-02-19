b9fbdd8fbf00ed404e8e27351e5aa21772cf87e8
Jovan Ellis <jovanellis31@gmail.com>
2024-02-19 17:26:00 +0000

    comment out plan for lambda handler

--- src/transform/python_to_parquet.py
+++ src/transform/python_to_parquet.py
@@ -3,7 +3,7 @@ import awswrangler as wr
 
 
 
-def python_to_parquet(table_data, s3, bucket_name, file_key):
+def python_to_parquet(table_data, bucket_name, timestamp_key):
     """
     Arg: 
     - table_data - new data created from unit functions in correct schema, form of python list of dictionaries
@@ -16,7 +16,7 @@ def python_to_parquet(table_data, s3, bucket_name, file_key):
     """
     table_name = list(table_data.keys())[0]
     dataframe = pd.DataFrame(table_data[table_name])
-    wr.s3.to_parquet(dataframe, path=f"s3://{bucket_name}/{file_key}/{table_name}.parquet", index=False)
+    wr.s3.to_parquet(dataframe, path=f"s3://{bucket_name}/{timestamp_key}/{table_name}.parquet", index=False)
     
 
     
