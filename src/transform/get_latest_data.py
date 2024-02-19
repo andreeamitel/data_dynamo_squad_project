e9d1949bad5fff6f3e2427c9ea90bf1fda165036
Jovan Ellis <jovanellis31@gmail.com>
2024-02-19 16:44:41 +0000

    start lambda handler 2

---
+++ src/transform/get_latest_data.py
@@ -0,0 +1,2 @@
+def get_latest_data(s3):
+    
--- src/transform/lambda_handler.py
+++ src/transform/lambda_handler.py
@@ -1,5 +1,6 @@
 import boto3
 import re
+
 def lambda_handler(event, context):
     """
     ### Args:
@@ -10,11 +11,18 @@ def lambda_handler(event, context):
             a valid AWS lambda Python context object - see
             https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
     ### Functionality:
-    - from ingestion bucket uses function get_latest_data()  
+    - uses function get_latest_data() to get data from ingestion bucket and convert to python  
     - passed into unit functions to change from OLTP to OLAP
     - calls python_to_parquet function -  which changes python to parquet and stores in processed bucket  
     """
     pass
-    list_of_tables = ["currency, sales"]  
+    sales_dict = get_latest_data("sales_order", s3, bucket_name)
+    currency_dict = get_latest_data("currency", s3, bucket_name)
+    design_dict = get_latest_data("design", s3, bucket_name)
+    address_dict = get_latest_data("address", s3, bucket_name)
+    counterparty_dict = get_latest_data("counterparty", s3, bucket_name)
+    staff_dict = get_latest_data("staff", s3, bucket_name)
+    department_dict = get_latest_data("department", s3, bucket_name)
+    
     
 lambda_handler(1,2)
