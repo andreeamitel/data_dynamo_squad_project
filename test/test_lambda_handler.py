9795e13e6f153f4b745ea8fbfb19d6856c49fc3e
NathStone <nathan_0803@hotmail.co.uk>
2024-02-20 13:42:55 +0000

    changed some tests to pass workflow

--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -108,7 +108,7 @@ def test_client_error_bucket(caplog,secretmanager):
 @pytest.mark.describe("lambda_handler")
 @pytest.mark.it("Error: ClientError - for secretsmanager")
 @mock_aws
-def test_client_error_secrets(caplog,create_bucket1, create_object):
+def test_client_error_secrets(caplog,create_bucket1, create_object, aws_secrets):
     with caplog.at_level(logging.INFO):
         lambda_handler("thing1", "thing2")
         assert "Secrets Manager can't find the specified secret." in caplog.text
