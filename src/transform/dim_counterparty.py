21ccf936a9883f200285257fb1559b21900dae84
Valerie Parfeliuk <parfelyuk.v@gmail.com>
2024-02-21 10:41:25 +0000

    refactored dim_counerparty and test

--- src/transform/dim_counterparty.py
+++ src/transform/dim_counterparty.py
@@ -2,13 +2,14 @@ import copy
 
 
 def dim_counterparty(address_table_data, counterparty_table_data):
-    """This function takes a two lists of dictionaries from the counterparty and address tables and returns amended list of dictionaries for the dim_counterparty table.
-    Args: 'counterparty' and 'address' table lists
-    Returns: dim_counterparty table list of dict
+    """This function takes a two dictionaries from the counterparty and address tables and returns amended dictionary with key = table name and value list for the dim_counterparty table.
+    Args: 'counterparty' and 'address' dictionaries
+    Returns: dim_counterparty dictionary
     """
-    address_table = copy.deepcopy(address_table_data)
-    counterparty_table = copy.deepcopy(counterparty_table_data)
+    address_table = copy.deepcopy(address_table_data['address'])
+    counterparty_table = copy.deepcopy(counterparty_table_data['counterparty'])
     dim_counterparty_list = []
+
     for counterparty, address in zip(counterparty_table, address_table):
         if address["address_id"] == counterparty["legal_address_id"]:
             dim_counterparty = {
@@ -20,7 +21,8 @@ def dim_counterparty(address_table_data, counterparty_table_data):
                 "counterparty_legal_city": address["city"],
                 "counterparty_legal_postal_code": address["postal_code"],
                 "counterparty_legal_country": address["country"],
-                "counterparty_legal_phone_number": address["phone"]
+                "counterparty_legal_phone_number": address["phone"],
             }
             dim_counterparty_list.append(dim_counterparty)
-    return dim_counterparty_list
+    return {"dim_counterparty": dim_counterparty_list}
+[]
