1cb33aa0a0963526990565f4975b591a5ee3a506
Valerie Parfeliuk <parfelyuk.v@gmail.com>
2024-02-21 10:50:48 +0000

    refactored dim_design and tests

--- src/transform/dim_design.py
+++ src/transform/dim_design.py
@@ -1,18 +1,19 @@
 import copy
 
-def dim_design(table_list):
+
+def dim_design(table_dict):
     """
-    This function takes a list of dictionary from the design table and return amended list of dictionaries for the dim_design table.
+    This function takes a dictionary from the design table and return amended  dictionary for the dim_design table.
 
     Args:
     'design' table_list
 
     Returns:
-    dim_design table list of dict
+    dim_design table dict
 
     """
-    dim_design_table = copy.deepcopy(table_list)
+    dim_design_table = copy.deepcopy(table_dict["design"])
     for design in dim_design_table:
         del design["created_at"]
         del design["last_updated"]
-    return dim_design_table
+    return {"dim_design": dim_design_table}
