2d6d89193d0e549a71f2ed0fd68fe3676b7fcc5f
bnjmn23 <ward.bnjmn@gmail.com>
2024-02-23 16:03:29 +0000

    add crazy code to figure out psql upload
    Co-authored-by: Valeriia <parfelyuk.v@gmail.com>

--- test/load/test_lambda_handler.py
+++ test/load/test_lambda_handler.py
@@ -44,67 +44,14 @@ def create_parquet_file(create_bucket1):
 def mock_conn():
     with patch("src.load.lambda_handler.Connection") as conn:
         yield conn
-@pytest.fixture
-def rds():
-    conn = boto3.client("rds", region_name="eu-west-2")
-    database = conn.create_db_instance(
-        DBInstanceIdentifier="db-master-1",
-        AllocatedStorage=10,
-        Engine="postgres",
-        DBName="testdb",
-        DBInstanceClass="db.m1.small",
-        LicenseModel="license-included",
-        MasterUsername="root",
-        MasterUserPassword="hunter2",
-        Port=1234,
-        DBSecurityGroups=["my_sg"],
-        VpcSecurityGroupIds=["sg-123456"],
-    )
-    db_instance = database["DBInstance"]
-    # pprint(db_instance)
 
 @pytest.mark.describe("lambda_handler")
 @pytest.mark.it("should read one file")
-@patch("src.load.lambda_handler.wr.sqlserver.to_sql")
-def test_read_one_file(to_sql, create_parquet_file, mock_conn, rds):
-    result = lambda_handler('event', 'context')
+def test_read_one_file(create_parquet_file):
+    lambda_handler('event', 'context')
     # print(isinstance(create_parquet_file, pd.DataFrame))
-    mock_parquet = wr.s3.read_parquet(
-        "s3://processed-bucket-20240222143124212400000004/2022-02-14 16:54:36.774180/fact_sales_order.parquet"
-    )
-    to_sql.assert_called_once_with(mock_parquet)
+    
+    
 
 
-
-# @mock_aws
-# class TestData(unittest.TestCase):
-#     def setUp(self):
-#         """Initial setup."""
-#         # Setup db
-
-#         test_instances = db_conn.create_db_instance(
-#             DBName='test_db',
-#             AllocatedStorage=10,
-#             StorageType='standard',
-#             DBInstanceIdentifier='instance',
-#             DBInstanceClass='db.t2.micro',
-#             Engine='postgres',
-#             MasterUsername='postgres_user',
-#             MasterUserPassword='p$ssw$rd',
-#             AvailabilityZone='us-east-1',
-#             PubliclyAccessible=True,
-#             DBSecurityGroups=["my_sg"],
-#             VpcSecurityGroupIds=["sg-123456"],
-#             Port=5432
-#         )
-#         db_instance = test_instances["DBInstance"]
-
-#         user_name = db_instance['MasterUsername']
-#         host = db_instance['Endpoint']['Address']
-#         port = db_instance['Endpoint']['Port']
-#         db_name = db_instance['DBName']
-#         conn_str = f'postgresql://{user_name}:p$ssw$rd@{host}:{port}/{db_name}'
-#         print(conn_str)
-#         engine_con = create_engine(conn_str)
-#         engine_con.connect()
 
