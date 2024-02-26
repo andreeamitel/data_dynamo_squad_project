a29a58e7e4e735b9d55b1e43c14a7625b61832b3
Valerie Parfeliuk <parfelyuk.v@gmail.com>
2024-02-26 18:19:00 +0000

    refactored lambda_handler and tests

--- test/load/test_lambda_handler.py
+++ test/load/test_lambda_handler.py
@@ -5,56 +5,10 @@ import boto3
 from moto import mock_aws
 from unittest.mock import patch, Mock
 import json
-import pandas as pd
-import pprint
 import awswrangler as wr
-from sqlalchemy import Numeric
-from decimal import Decimal,getcontext
-json_with_datatypes =  {"sales_order": [
-        {
-            "sales_order_id": 1,
-            "created_at": "2022-11-03T14:20:52.186",
-            "last_updated": "2022-11-03T14:20:52.186",
-            "design_id": 9,
-            "staff_id": 16,
-            "counterparty_id": 18,
-            "units_sold": 84754,
-            "unit_price":  Decimal(2.43),
-            "currency_id": 3,
-            "agreed_delivery_date": "2022-11-10",
-            "agreed_payment_date": "2022-11-03",
-            "agreed_delivery_location_id": 4,
-        },
-        {
-            "sales_order_id": 2,
-            "created_at": "2022-11-03T14:20:52.186",
-            "last_updated": "2022-11-03T14:20:52.186",
-            "design_id": 3,
-            "staff_id": 19,
-            "counterparty_id": 8,
-            "units_sold": 42972,
-            "unit_price": Decimal(2.43),
-            "currency_id": 2,
-            "agreed_delivery_date": "2022-11-07",
-            "agreed_payment_date": "2022-11-08",
-            "agreed_delivery_location_id": 8,
-        },
-        {
-            "sales_order_id": 3,
-            "created_at": "2022-11-03T14:20:52.188",
-            "last_updated": "2022-11-03T14:20:52.188",
-            "design_id": 4,
-            "staff_id": 10,
-            "counterparty_id": 4,
-            "units_sold": 65839,
-            "unit_price": Decimal(2.43),
-            "currency_id": 3,
-            "agreed_delivery_date": "2022-11-06",
-            "agreed_payment_date": "2022-11-07",
-            "agreed_delivery_location_id": 19,
-        },
-    ]
-}
+import logging
+from pg8000.native import DatabaseError
+
 
 @pytest.fixture(scope="function")
 def aws_s3():
@@ -86,18 +40,151 @@ def create_parquet_file(create_bucket1):
             "2022-02-14 16:54:36.774180",
         )
 
+
+@pytest.fixture
+def create_last_processed_file(create_bucket1):
+    s3 = boto3.client("s3")
+    s3.upload_file(
+        "./test/load/Last_Processed.txt",
+        "processed-bucket-20240222143124212400000004",
+        "Last_Processed.txt",
+    )
+    s3.upload_file(
+        "test/load/dim_address/2024-02-25T14_01_42.316404.parquet",
+        "processed-bucket-20240222143124212400000004",
+        "dim_address/2022-02-14 16:54:36.774180.parquet",
+    )
+    s3.upload_file(
+        "test/load/dim_address_copy/2024-02-25T14_01_42.316404.parquet",
+        "processed-bucket-20240222143124212400000004",
+        "dim_address_copy/2024-02-25T14_01_42.316404.parquet",
+    )
+
+
+@pytest.fixture
+def secretmanager(aws_secrets):
+    aws_secrets.create_secret(
+        Name="database_creds_test",
+        SecretString='{"hostname":"example_host.com","port": "4321", "database" : "example_database", "username": "project_team_0", "password":"EXAMPLE-PASSWORD"}',
+    )
+    aws_secrets.create_secret(
+        Name="processed_bucket3",
+        SecretString="processed-bucket-20240222143124212400000004",
+    )
+
+
 @pytest.fixture
 def mock_conn():
-    with patch("src.load.lambda_handler.Connection") as conn:
+    with patch("src.load.lambda_handler.pg8000.connect") as conn:
         yield conn
+
+
+@pytest.mark.describe("lambda_handler")
+@pytest.mark.it(
+    "Test that a connection has been established to a database - using secretsmanager"
+)
+@mock_aws
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+def test_database_conn(
+    mock_to_sql,
+    mock_conn,
+    create_bucket1,
+    secretmanager,
+    create_parquet_file,
+    create_last_processed_file,
+):
+    mock_to_sql.return_value = 3
+    lambda_handler("thing1", "thing2")
+    mock_conn.assert_called_with(
+        host="example_host.com",
+        port="4321",
+        database="example_database",
+        user="project_team_0",
+        password="EXAMPLE-PASSWORD",
+    )
+
 
 @pytest.mark.describe("lambda_handler")
 @pytest.mark.it("should read one file")
-def test_read_one_file(create_parquet_file):
-    lambda_handler('event', 'context')
-   
-    
-    
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+def test_read_one_file(
+    mock_to_sql,
+    secretmanager,
+    create_parquet_file,
+    create_last_processed_file,
+    mock_conn,
+    caplog,
+):
+    mock_to_sql.return_value = 3
+    with caplog.at_level(logging.INFO):
+        lambda_handler("event", "context")
+        expected = "Successfully inserted 3 rows into fact_sales_order table"
+        assert expected in caplog.text
 
 
+@pytest.mark.describe("lambda_handler")
+@pytest.mark.it("should read one file")
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+def test_ignore_timestamp(
+    mock_to_sql,
+    secretmanager,
+    create_parquet_file,
+    create_last_processed_file,
+    mock_conn,
+    caplog,
+):
+    mock_to_sql.return_value = 3
+    with caplog.at_level(logging.INFO):
+        lambda_handler("event", "context")
+        expected = "Successfully inserted 3 rows into dim_address_copy table"
+        assert expected not in caplog.text
 
+
+@pytest.mark.describe("lambda_handler")
+@pytest.mark.it("should read one file")
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+def test_client_error(
+    mock_to_sql, create_parquet_file, create_last_processed_file, mock_conn, caplog
+):
+    mock_to_sql.return_value = 3
+    with caplog.at_level(logging.INFO):
+        lambda_handler("event", "context")
+        expected = "ClientError: ResourceNotFoundException: Secrets Manager can't find the specified secret."
+        assert expected in caplog.text
+
+
+@pytest.mark.describe("lambda_handler")
+@pytest.mark.it("should read one file")
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+@patch("src.load.lambda_handler.pg8000.connect")
+def test_ignore_timestamp_database_error(
+    mock_to_sql,
+    mock_fail_conn,
+    secretmanager,
+    create_parquet_file,
+    create_last_processed_file,
+    caplog,
+):
+    mock_fail_conn.side_effect = DatabaseError()
+    mock_to_sql.return_value = 3
+    with caplog.at_level(logging.INFO):
+        lambda_handler("event", "context")
+        expected = 'DatabaseError'
+        assert expected in caplog.text
+
+
+@pytest.mark.it("should read one file")
+@patch("src.load.lambda_handler.wr.postgresql.to_sql")
+def test_ignore_timestamp_sql_error(
+    mock_to_sql,
+    mock_conn,
+    secretmanager,
+    create_parquet_file,
+    create_last_processed_file,
+    caplog,
+):
+    mock_to_sql.side_effect = Exception({'S': 'ERROR', 'V': 'ERROR', 'C': '22P02', 'M': 'invalid input syntax for type integer: "2.43"', 'F': 'numutils.c', 'L': '323', 'R': 'pg_strtoint32'})
+    with caplog.at_level(logging.INFO):
+        lambda_handler("event", "context")
+        expected = "invalid input"
+        assert expected in caplog.text
