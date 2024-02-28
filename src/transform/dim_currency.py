e3de427e53c2f34fd948b3e27e85a8fbf4d976c5
Sumeyah <sumiibra17@icloud.com>
2024-02-28 15:53:18 +0000

    refactor dim_currency

--- src/transform/dim_currency.py
+++ src/transform/dim_currency.py
@@ -1,16 +1,23 @@
-import pandas as pd
-
-
 def dim_currency(currency_df):
-    if currency_df == {}:
-        return {}
+    """
+    This function takes one dataframe,
+    currency and  returns
+    dim_currency dataframe.
+
+    Args:
+    'currency' dataframe
+    Returns:
+    dim_currency dataframe
+    """
     currency_copy = currency_df.copy(deep=True)
     currency_copy = currency_copy.drop(columns=["created_at", "last_updated"])
     currency_code_name = {
         "GBP": "Great British Pound",
         "USD": "United States Dollar",
-        "EUR": "Euro"
+        "EUR": "Euro",
     }
-    currency_copy["currency_name"] = currency_copy["currency_code"].map(currency_code_name)
+    currency_copy["currency_name"] = currency_copy["currency_code"].map(
+        currency_code_name
+    )
 
     return currency_copy
--- test/transform/test_dim_currency.py
+++ test/transform/test_dim_currency.py
@@ -1,20 +1,11 @@
-"""Tests the function dim_currency."""
-
 import pandas as pd
 import pytest
 from src.transform.dim_currency import dim_currency
 
 
 @pytest.mark.describe("dim_currency")
-@pytest.mark.it("function returns an empty list if given empty table lists")
-def test_returns_empty_list_when_given_empty_dict():
-    result = dim_currency({})
-    assert result == {}
-
-
-@pytest.mark.describe("dim_currency")
-@pytest.mark.it("function returns a dictionary which has corrrect key")
-def test_returns_dict_with_correct_key():
+@pytest.mark.it("function returns a dictionary which has correct columns")
+def test_returns_data_frame_with_correct_columns():
 
     currency_df = {
         "currency": [
@@ -39,7 +30,6 @@ def test_returns_dict_with_correct_key():
         ]
     }
     df = pd.DataFrame(currency_df["currency"])
-    print(df)
     result = dim_currency(df)
     bool_list = [
         column in result
@@ -53,58 +43,47 @@ def test_returns_dict_with_correct_key():
 
 
 @pytest.mark.describe("dim_currency")
-@pytest.mark.it("function returns a dictionary which has corrrect values")
-def test_returns_dict_with_correct_values():
-    result = dim_currency(
-        {
-            "currency": [
-                {
-                    "currency_id": 1,
-                    "currency_code": "GBP",
-                    "created_at": "2022-11-03T14:20:49.962",
-                    "last_updated": "2022-11-03T14:20:49.962",
-                },
-                {
-                    "currency_id": 2,
-                    "currency_code": "USD",
-                    "created_at": "2022-11-03T14:20:49.962",
-                    "last_updated": "2022-11-03T14:20:49.962",
-                },
-                {
-                    "currency_id": 3,
-                    "currency_code": "EUR",
-                    "created_at": "2022-11-03T14:20:49.962",
-                    "last_updated": "2022-11-03T14:20:49.962",
-                },
-            ]
-        }
-    )
-    expected = {
-        "dim_currency": [
+@pytest.mark.it("function returns a data frame which has corrrect values")
+def test_returns_data_frame_with_correct_values():
+    currency_df = {
+        "currency": [
             {
                 "currency_id": 1,
                 "currency_code": "GBP",
-                "currency_name": "Great British Pound",
+                "created_at": "2022-11-03T14:20:49.962",
+                "last_updated": "2022-11-03T14:20:49.962",
             },
             {
                 "currency_id": 2,
                 "currency_code": "USD",
-                "currency_name": "United States Dollar",
+                "created_at": "2022-11-03T14:20:49.962",
+                "last_updated": "2022-11-03T14:20:49.962",
             },
             {
                 "currency_id": 3,
                 "currency_code": "EUR",
-                "currency_name": "Euro",
+                "created_at": "2022-11-03T14:20:49.962",
+                "last_updated": "2022-11-03T14:20:49.962",
             },
         ]
     }
-    assert result == expected
+    df = pd.DataFrame(currency_df["currency"])
+    result = dim_currency(df)
+    assert result.to_dict() == {
+        "currency_id": {0: 1, 1: 2, 2: 3},
+        "currency_code": {0: "GBP", 1: "USD", 2: "EUR"},
+        "currency_name": {
+            0: "Great British Pound",
+            1: "United States Dollar",
+            2: "Euro",
+        },
+    }
 
 
 @pytest.mark.describe("dim_currency")
 @pytest.mark.it("check result is different reference than input")
 def test_check_result_has_diff_ref():
-    test_input = {
+    currency_df = {
         "currency": [
             {
                 "currency_id": 1,
@@ -126,14 +105,15 @@ def test_check_result_has_diff_ref():
             },
         ]
     }
-    result = dim_currency(test_input)
-    assert result is not test_input
+    df = pd.DataFrame(currency_df["currency"])
+    result = dim_currency(df)
+    assert result is not currency_df
 
 
 @pytest.mark.describe("dim_currency")
 @pytest.mark.it("check input has not been changed")
 def test_check_input_not_changed():
-    test_input = {
+    currency_df = {
         "currency": [
             {
                 "currency_id": 1,
@@ -155,9 +135,10 @@ def test_check_input_not_changed():
             },
         ]
     }
-    dim_currency(test_input)
+    df = pd.DataFrame(currency_df["currency"])
+    dim_currency(df)
 
-    assert test_input == {
+    assert currency_df == {
         "currency": [
             {
                 "currency_id": 1,
