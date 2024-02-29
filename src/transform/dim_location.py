97141ac47c9539a6c8bbafc8f8ead7b3536e4306
bnjmn23 <ward.bnjmn@gmail.com>
2024-02-29 09:55:01 +0000

    change column

--- src/transform/dim_location.py
+++ src/transform/dim_location.py
@@ -12,11 +12,8 @@ def dim_location(address_df):
     dim_location_df = address_df.copy(deep=True)
     dim_location_df = dim_location_df.drop(
         columns=["created_at", "last_updated"])
-    dim_location_df = dim_location_df.rename(
-        columns={"address_id": "location_id"}
-    )
     dim_location_df = dim_location_df[
-        ["location_id",
+        ["address_id",
          "address_line_1",
          "address_line_2",
          "district",
